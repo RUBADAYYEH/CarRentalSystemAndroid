a37ef50aa595cd709f5dce5fbf0a769c7c4da6ca
YUNAAMIN <yunanawahdah2003@gmail.com>
2024-06-06 09:37:00 +0300

    sign in using volly

--- app/src/main/java/com/example/carrentalsystem/SignIn.java
+++ app/src/main/java/com/example/carrentalsystem/SignIn.java
@@ -1,30 +1,31 @@
 package com.example.carrentalsystem;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import android.app.ProgressDialog;
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignIn extends AppCompatActivity {
 
-    EditText emailEditText;
-    EditText passwordEditText;
-    Button signInButton;
-    private ProgressDialog progressDialog;
-
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button signInButton;
+    private RequestQueue requestQueue;
+    private static final String URL = "http://xampp/htdocs/signin.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,98 +35,64 @@ public class SignIn extends AppCompatActivity {
         emailEditText = findViewById(R.id.editText_email);
         passwordEditText = findViewById(R.id.editText_password);
         signInButton = findViewById(R.id.button_signIn);
-        progressDialog = new ProgressDialog(this);// progress dialog
-        progressDialog.setMessage("Signing in occuring ...");
-        progressDialog.setCancelable(false);
 
-        signInButton.setOnClickListener(new View.OnClickListener() {// set on action when click login
+        requestQueue = Volley.newRequestQueue(this);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = emailEditText.getText().toString().trim();
                 String password = passwordEditText.getText().toString().trim();
 
                 if (!email.isEmpty() && !password.isEmpty()) {
-                    new SignInTask().execute(email, password);
+                    signIn(email, password);
                 } else {
-                    Toast.makeText(SignIn.this, "Please fill in all the fields", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignIn.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (progressDialog != null && progressDialog.isShowing()==true) {
-            progressDialog.dismiss();
-        }
-    }
-    private class SignInTask extends AsyncTask<String, Void, String> {
+    private void signIn(final String email, final String password) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            boolean success = obj.getBoolean("success");
 
-        @Override
-        protected String doInBackground(String... params) {
-            String email = params[0];
-            String password = params[1];
-            String response = "";
-
-            try {
-                URL url = new URL("http://xampp/htdocs/signin.php");
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("POST");
-                conn.setDoOutput(true);
-
-                String postData = "email=" + email + "&password=" + password;
-                OutputStream os = conn.getOutputStream();
-                os.write(postData.getBytes());
-                os.flush();
-                os.close();
-
-                int responseCode = conn.getResponseCode();
-
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    String line;
-                    StringBuilder sb = new StringBuilder();
-
-                    while ((line = in.readLine()) != null) {
-                        sb.append(line);
+                            if (success) {
+                                Intent intent = new Intent(SignIn.this, MainActivityForUser.class);
+                                startActivity(intent);
+                                finish();
+                                Toast.makeText(SignIn.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                String message = obj.getString("message");
+                                Toast.makeText(SignIn.this, message, Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(SignIn.this, "Error parsing JSON", Toast.LENGTH_SHORT).show();
+                        }
                     }
-
-                    response = sb.toString();
-                    in.close();
-                } else {
-                    response = "Error occuer: " + responseCode;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("SignIn", "Error occures!! : " + error.getMessage());
+                        Toast.makeText(SignIn.this, "Sign in is fail!", Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", password);
+                return params;
             }
+        };
 
-            return response;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            try {
-                JSONObject obj = new JSONObject(result);
-                boolean success = obj.getBoolean("success");
-
-                if (success==false) {
-                    String message = obj.getString("message");
-                    Toast.makeText(SignIn.this, message, Toast.LENGTH_SHORT).show();
-
-                } else {
-                    Intent intent = new Intent(SignIn.this, MainActivityForUser.class);
-                    startActivity(intent);
-                    finish();
-                    Toast.makeText(SignIn.this, "Sign in done successfully", Toast.LENGTH_SHORT).show();
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Toast.makeText(SignIn.this, "Error parsing the json file", Toast.LENGTH_SHORT).show();
-            }
-        }
+        requestQueue.add(stringRequest);
     }
 }
-
